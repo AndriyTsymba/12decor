a70e4b61035b14b0c5644aaad63b69852cea2095
student <artembermnat@gmail.com>
2023-05-02 20:18:01 +0300

    2

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
-#Теорія Декоратори
+#1
 
 
-def my_decorator_func(func):#3
-  def wrapper():#4
-      print("шото там")
-      func()
-      print("і шо там")
-  return wrapper
-@my_decorator_func#2
-def say_helow():#1
-     print("hello")
-say_helow()     
 #2
 import time
-def delay_decorator(func):
-  def wrapper(*args,**kwargs):
-      time.sleep(3)
-      return func(*args,**kwargs)
-  return wrapper
-@delay_decorator
-def sleepy():
-    print("я сплю")
-sleepy()
-#3
+def time_fuction(func):
+    def wrapper(*args,**kwargs):
+        start_time = time.time()
+        result = func(*args,**kwargs)
+        end_time = time.time()
+        print("Час виконнання функції",end_time - start_time)
+        return result
+    return wrapper()
+@time_fuction
+def example_fuction(x):
+    time.sleep(1)#pass
+    return x
+print(example_fuction(56))
