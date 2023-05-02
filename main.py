b7e0623d3ea1978adae3ded4d215d2b52aa59963
student <artembermnat@gmail.com>
2023-05-02 18:41:39 +0300

    прерший приклад обгортки функції

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-
+#Теорія Декоратори
+def my_decorator_func(func):#3
+  def wrapper():#4
+      print("шото там")
+      func()
+      print("і шо там")
+  return wrapper
+@my_decorator_func#2
+def say_helow():#1
+     print("hello")
+say_helow()     
